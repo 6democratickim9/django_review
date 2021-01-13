994a12211c05fb0e2ab7ffd0c492d58de300f1a8
6democratickim9 <shcomon24@gmail.com>
2021-01-13 20:57:31 +0900

    update

--- django_re/urls.py
+++ django_re/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from product import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url('',include('blog.urls')),
+
 ]
--- product/models.py
+++ product/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-from django.utils import
-# Create your models here.
+from django.utils import timezone
+
+
+class Post(models.Model):
+    name = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    productname = models.CharField(max_length=200)
+    price = models.BigIntegerField()
+    stock_quatity = models.BigIntegerField()
+    description = models.TextField
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
